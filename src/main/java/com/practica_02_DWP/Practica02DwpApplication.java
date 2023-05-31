5661d1ba6a8cd18fcea865913e57e62d02e34137
Ch4rl <Ch4rl@LAPTOP-GV845E6E>
2023-05-31 11:05:31 -0600

    Creacion del proyecto para la practica 2 de Desarrollo Web y Patrones

---
+++ src/main/java/com/practica_02_DWP/Practica02DwpApplication.java
@@ -0,0 +1,13 @@
+package com.practica_02_DWP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica02DwpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica02DwpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica_02_DWP/Practica02DwpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica_02_DWP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica02DwpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
